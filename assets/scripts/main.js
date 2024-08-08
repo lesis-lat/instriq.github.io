632d98aacf667c921a6a9eb7a77d2483c8db224b
htrgouvea <hi@heitorgouvea.me>
2024-08-08 13:13:19 -0300

    add a contact form

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,3 +3,29 @@ var host="lesis.lat"
 if (window.location.host == host && window.location.protocol != "https:") {
     window.location.protocol = "https:"
 }
+
+// Get the modal
+var modal = document.getElementById("contactModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("contactBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
